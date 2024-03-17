219376d8013b393c0976bf894e5917e6659b52d0
hejzbh <hazimba123@gmail.com>
2024-03-17 22:26:04 +0100

    Error msg for use invite code

--- constants/errorMessages.ts
+++ constants/errorMessages.ts
@@ -6,5 +6,5 @@ export const CREATE_SERVER_ERROR = "Something went wrong";
 export const CREATE_SERVER_MISSING_DATA_ERROR = "Data is not provided";
 
 // Invite
-export const USE_INVITE_CODE_ERROR = "Something went wrong";
+export const USE_INVITE_CODE_ERROR = "Something went wrong or invalid code";
 export const USE_INVITE_CODE_MISSING_ERROR = "Invite code is missing";
