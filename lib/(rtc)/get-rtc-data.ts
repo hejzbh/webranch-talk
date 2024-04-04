a41db339aa20f35ff5f5bbbf590e221807c9cbf0
hejzbh <hazimba123@gmail.com>
2024-04-04 23:57:05 +0200

    edit

--- lib/(rtc)/get-rtc-data.ts
+++ lib/(rtc)/get-rtc-data.ts
@@ -7,8 +7,8 @@ export const getRtcData = ({
   channelID: string;
   currentAccountID: string;
 }) => {
-  const appID = process.env.NEXT_PUBLIC_AGORA_APP_ID!;
-  const appCertificate = process.env.NEXT_PUBLIC_AGORA_CERTIFICATE!;
+  const appID = "aef2fad1905a4be0b65091b66cb47a67";
+  const appCertificate = "c34f874396e64061bdb7f1852d9e0ca5";
   const channel = channelID;
   const role = RtcRole.PUBLISHER;
   const expirationTimeInSeconds = 3600;
